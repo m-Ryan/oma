f598042126f8f1da8a85ad1238c31352c7855500
maocanhua <962491243@qq.com>
2020-04-06 01:21:44 +0800

    docker

--- src/deployment/ProjectTask.ts
+++ src/deployment/ProjectTask.ts
@@ -14,12 +14,6 @@ import { SSHEntity, SSHType } from '../modules/project/entities/ssh.entity';
 import chalk from 'chalk';
 import { decrypt } from '../utils/crypto';
 
-/**
- * 假设分三个环境 master，dev，test
- * master dev 和 test 通过 push 的钩子完成编译，除master分支外自动部署，master分支不会自动部署
- * master编译保留记录，并保留最近10次编译的bundle
- */
-
 export async function createBuildPipline(options: {
   task: ProjectTaskEntity;
   onProgress: (log: string) => void;
